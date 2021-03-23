9acfb1c2ee85070d9324139960bc0c12348c4214
Johnny Chen <johnnychen94@hotmail.com>
2021-03-23 21:32:27 +0800

    WIP: build and bind libgreet in Julia

--- greet/greet.cpp
+++ greet/greet.cpp
@@ -1,6 +1,13 @@
-#include "greet.hpp"
+#include <string>
+
+#include "jlcxx/jlcxx.hpp"
 
 std::string greet()
 {
    return "hello, world";
+}
+
+JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
+{
+  mod.method("greet", &greet);
 }
